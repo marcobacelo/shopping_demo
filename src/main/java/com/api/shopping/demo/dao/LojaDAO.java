000cb845637f8b869a78467affa1188ece8c0933
Marco Bacelo <marcobacelo90@gmail.com>
2020-07-14 05:03:10 -0300

    finished dao repository

--- src/main/java/com/api/shopping/demo/dao/LojaDAO.java
+++ src/main/java/com/api/shopping/demo/dao/LojaDAO.java
@@ -4,12 +4,8 @@ import com.api.shopping.demo.model.Loja;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface LojaDAO extends JpaRepository<Loja, Long> {
 
     Loja findLojaByCnpj(int cnpj);
-
-    Loja findLojaByLoja_number(int id);
 }
