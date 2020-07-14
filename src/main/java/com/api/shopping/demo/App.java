c5469b50e2c6cf21c52dbb90747dfae9c37d783f
Marco Bacelo <marcobacelo90@gmail.com>
2020-07-13 22:54:46 -0300

    :art:created app skeleton

---
+++ src/main/java/com/api/shopping/demo/App.java
@@ -0,0 +1,20 @@
+package com.api.shopping.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+}
